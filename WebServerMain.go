7ea13d9a4db50add7ec7f31a6210d341dd940aba
Robin Coppens <robincoppens@hotmail.com>
2023-12-09 23:28:35 +0100

    using http responsewriter and using a normal way for the usage of packages

--- WebServerMain.go
+++ WebServerMain.go
@@ -3,17 +3,20 @@ package main
 import (
 	"fmt"
 	"math"
+	"mywebserver/morestrings"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, Go Web Server!")
+	fmt.Fprintf(w, "Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Fprintln(w, "But a bitch aint one")
+	fmt.Fprintln(w, morestrings.ReverseRunes("!oG ,olleH"))
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Server is running on http://localhost:8080")
-	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
-	fmt.Println("But a bitch aint one")
+
 	http.ListenAndServe(":8080", nil)
 }
